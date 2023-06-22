6c60c13d8a6b3054a8eb931101f82619ecf78a78
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-06-22 19:43:06 +0300

    add new css

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,14 +1,13 @@
 
 
-function MenuItem({ image, name , price }) {
+function MenuItem({ image, name, price }) {
   return (
     <div className="menuItem">
-      <div style={{ backgroundImage: `url(${image})`}}>
-        <h1> {name} </h1>
-        <p> ${price} </p>
-      </div>
+      <div style={{ backgroundImage: `url(${image})` }}> </div>
+      <h1> {name} </h1>
+      <p> ${price} </p>
     </div>
-  )
+  );
 }
 
-export default MenuItem
+export default MenuItem;
---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import IrisBacker from '../assets/backer1.jpeg';
+import "../styles/About.css";
+
+function About() {
+  return (
+    <div className="about">
+        <div className="aboutTop"
+    style={{ backgroundImage: `url(${IrisBacker})` }}
+
+        ></div>
+        <div className="aboutBottom">
+            <h1>ABOUT US</h1>
+            <p> <b>Уникальный вкус</b> <br />
+«Очень легкий и не слишком насыщенный» 
+Свежие фрукты, свежие яйца, чистые взбитые сливки и сладкое масло (без искусственных ароматизаторов и консервантов) делают вкус торта Iris Backer таким же изысканным, как и внешний вид. Но хотя они выглядят невероятно богатыми, вкус удивительно легкий <br />и нюансированный. В сочетании с бесконечным выбором слоев торта, глазури и начинки уникальный  тонкий вкус сделает свадебный торт Iris Backer незабываемым для всех в вашем списке гостей. </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
